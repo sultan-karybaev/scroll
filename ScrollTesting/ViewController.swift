0281877e81bede55f505e481ef06e170a2b3afea
Sultan Karybaev <sultankarybaev@macbook-air-sultan.local>
2018-07-08 23:07:56 +0600

    Initial Commit

---
+++ ScrollTesting/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScrollTesting
+//
+//  Created by Sultan Karybaev on 08.07.2018.
+//  Copyright © 2018 Sultan Karybaev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
